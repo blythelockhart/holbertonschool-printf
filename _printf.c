c3d547b76ef2f9498cada79cd2e6960723c7f44e
dillonjgage <dillonjgage1@gmail.com>
2023-07-08 12:15:27 -0700

    updated documentation

--- _printf.c
+++ _printf.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
+ * _printf - selects the correct function to print
+ * @format: format identifier to look for
  *
- *
- *
+ * Return: num of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	convert_match m[] =
@@ -11,18 +14,29 @@ int _printf(const char *format, ...)
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%i", printf_int}, {"%d", printf_dec}, {"%%", printf_37}
 		/**
-		 * Array 'm' contains different format specifiers and
-		 * their corresponding functions to handle each
-		 * type of formatting.
+		 * Array 'm' containing structures that define different
+		 * format specifiers that our function will handle.
+		 * Each contain a pointer to the format specifier string and
+		 * a pointer to the function that will be called to print the
+		 * value of the arg.
 		 */
 	};
 
 	va_list args;
 	int i = 0, j, len = 0;
+	/**
+	 * 'args' to handle the variable num of args given
+	 * variables given to help iterate throughout the
+	 * format string and args
+	 */
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	/**
+	 * Return: -1 if format string is NULL or
+	 * if it only contains a single % character.
+	 */
 
 Here:
 	while (format[i] != '\0')
@@ -41,7 +55,18 @@ Here:
 		_putchar(format[i]);
 		len++;
 		i++;
+		/**
+		 * Loop to iterate throughout the format
+		 * string one character at a time.
+		 * Checks if the character is a format specifier (1 of 5):
+		 * 	if yes - call the appropriate func
+		 * 	if no - print character to stdout
+		 */
 	}
 	va_end(args);
 	return (len);
+	/**
+	 * clean up the variable arg list
+	 * Return: num of characters printed
+	 */
 }
