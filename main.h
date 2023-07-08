f1417898043a6adb73bc6c5a50a1f335c180846a
dillonjgage <dillonjgage1@gmail.com>
2023-07-08 11:45:21 -0700

    updated header with new prototypes and libraries

--- main.h
+++ main.h
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int printf_37(void);
+int printf_int(va_list args);
+int printf_dec(va_list args);
 
 #endif
