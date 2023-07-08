7ee62b8d98d3e444461e26dcba81f2b36d638090
dillonjgage <dillonjgage1@gmail.com>
2023-07-08 11:43:21 -0700

    identified base prototypes for formatting functions

--- print_functions.c
+++ print_functions.c
@@ -1,1 +1,48 @@
 #include "main.h"
+
+/**
+ * printf_char - prints a character
+ * @val: the argument list passed in.
+ *
+ * Return: 1, the character length.
+ */
+int printf_char(va_list val)
+{
+}
+
+/**
+ * printf_string - prints a string as arg to the printf
+ * @val: the argument list passed in.
+ *
+ * Return: the length of the string.
+ */
+int printf_string(va_list val)
+{
+}
+
+/**
+ * printf_37 - prints the % character
+ *
+ * Return: 1, which is the length of %;
+ */
+int printf_37(void)
+{
+}
+
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_int(va_list args)
+{
+}
+
+/**
+ * printf_dec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_dec(va_list args)
+{
+}
