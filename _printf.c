b11afe62fb00fbe3ea974d9fd84d1012f8b40f5d
dillonjgage <dillonjgage1@gmail.com>
2023-07-08 11:55:21 -0700

    added functionality to move throughout input and format each element

--- _printf.c
+++ _printf.c
@@ -16,3 +16,32 @@ int _printf(const char *format, ...)
 		 * type of formatting.
 		 */
 	};
+
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 4;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
